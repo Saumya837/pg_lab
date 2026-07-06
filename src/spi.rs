6a7259500b31e19934299b6ecc17608af6b951f0
Somya Kumar <33091016+Saumya837@users.noreply.github.com>
2026-07-06 21:48:55 +0530

    step-12 parameterized SPI with quote_ident to prevent SQL injection
     pg_lab_row_count using SPI to run SELECT count(*)

--- src/spi.rs
+++ src/spi.rs
@@ -2,8 +2,12 @@ use pgrx::prelude::*;
 
 #[pg_extern]
 pub fn pg_lab_row_count(table_name : &str) -> Option<i64> {
-    let query = format!("select count(*) from {}", table_name);
-
+     let safe_name = Spi::get_one_with_args::<String>(
+        "SELECT quote_ident($1)",
+        &[table_name.into()]
+    ).unwrap().unwrap();
+    
+    // Then use it in the query
+    let query = format!("SELECT count(*) FROM {}", safe_name);
     Spi::get_one::<i64>(&query).unwrap()
-
 }
